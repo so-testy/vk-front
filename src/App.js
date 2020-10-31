1bf0dddd254e743b8192bf9bb6cf6bb9c840e5e2
Ladanov Nikolay <ladanov_n@mail.ru>
2020-10-31 16:37:14 +0300

    replace from App notification Modal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React, { useState } from 'react';
-import View from '@vkontakte/vkui/dist/components/View/View';
 import {
-    ModalRoot,
     Epic,
     Tabbar,
+    View,
     TabbarItem,
-    PanelHeaderButton,
-    ModalPage,
-    ModalPageHeader,
-    IS_PLATFORM_ANDROID,
-    IS_PLATFORM_IOS,
-    FormLayoutGroup,
-    FormLayout,
-    Checkbox,
-    ActionSheet,
-    ActionSheetItem,
-    CellButton,
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
@@ -28,8 +16,6 @@ import NavigationContext from './NavigationContext';
 import Exercise from './panels/Course/exercise/Exercise';
 import AuthContext from './AuthProvider';
 import {
-    Icon24Cancel,
-    Icon24Dismiss,
     Icon28Ghost,
     Icon28User,
 } from '@vkontakte/icons';
@@ -46,94 +32,6 @@ const App = () => {
 
     const [course, setCourse] = useState(null);
     const [exercise, setExercise] = useState(null);
-
-    const [isNotificationsEnabled, setNotifications] = useState(false);
-    const [hours, setHours] = useState(2);
-
-    const [popout, setPopout] = useState(null);
-
-    const modal = (
-        <ModalRoot activeModal={activeModal}>
-            <ModalPage
-                id="settings"
-                header={
-                    <ModalPageHeader
-                        left={
-                            IS_PLATFORM_ANDROID && (
-                                <PanelHeaderButton
-                                    onClick={() => setActiveModal(null)}
-                                >
-                                    <Icon24Cancel />
-                                </PanelHeaderButton>
-                            )
-                        }
-                        right={
-                            IS_PLATFORM_IOS && (
-                                <PanelHeaderButton
-                                    onClick={() => setActiveModal(null)}
-                                >
-                                    <Icon24Dismiss />
-                                </PanelHeaderButton>
-                            )
-                        }
-                    >
-                        Настройки курсов
-                    </ModalPageHeader>
-                }
-                onClose={() => setActiveModal(null)}
-                settlingHeight={700}
-                dynamicContentHeight
-            >
-                <FormLayout>
-                    <Checkbox
-                        checked={isNotificationsEnabled}
-                        onChange={() =>
-                            setNotifications(!isNotificationsEnabled)
-                        }
-                    >
-                        Присылать уведомления
-                    </Checkbox>
-                    {isNotificationsEnabled && (
-                        <FormLayoutGroup top="Периодичность уведомлений">
-                            <CellButton
-                                onClick={() => {
-                                    setPopout(
-                                        <ActionSheet
-                                            onClose={() => setPopout(null)}
-                                        >
-                                            <ActionSheetItem
-                                                autoclose
-                                                onClick={() => setHours(2)}
-                                            // before={<Icon28Profile />}
-                                            >
-                                                2 часа
-                                            </ActionSheetItem>
-                                            <ActionSheetItem
-                                                autoclose
-                                                onClick={() => setHours(3)}
-                                            // before={<Icon28CameraOutline />}
-                                            >
-                                                3 часа
-                                            </ActionSheetItem>
-                                            <ActionSheetItem
-                                                autoclose
-                                                onClick={() => setHours(4)}
-                                            // before={<Icon28CameraOutline />}
-                                            >
-                                                4 часа
-                                            </ActionSheetItem>
-                                        </ActionSheet>,
-                                    );
-                                }}
-                            >
-                                Каждые {hours} часа
-                            </CellButton>
-                        </FormLayoutGroup>
-                    )}
-                </FormLayout>
-            </ModalPage>
-        </ModalRoot>
-    );
 
     return (
         <AuthContext.Provider value={{ user: fetchedUser }}>
@@ -171,9 +69,7 @@ const App = () => {
                 }>
                     <View
                         id="courses"
-                        modal={modal}
                         activePanel={activePanel}
-                        popout={popout}
                     >
                         <Courses
                             id="courses"
